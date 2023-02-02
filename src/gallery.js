e95308de58b85778c0bce45659cc7e219d2dcc6b
Dmytro1001 <31dmitri13@gmail.com>
2023-02-02 11:32:10 +0200

    tst-work

--- src/gallery.js
+++ src/gallery.js
@@ -2,6 +2,6 @@ import './js/preloader';
 import './js/footer-modal';
 import './js/local-storage';
 import './js/dark-light';
-import './js/modalMovieLibrary';
-// import './js/modalMovieLibrarySS';
+// import './js/modalMovieLibrary';
+import './js/modalMovieLibrarySS';
 import './js/showing-trailer';
